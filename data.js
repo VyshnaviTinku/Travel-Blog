22c7b5c428ad18a912a9c2be16846de4e4251ce0
VyshnaviTinku <123009110+VyshnaviTinku@users.noreply.github.com>
2025-05-25 14:46:52 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,106 @@
+//data.js
+import img1 from './images/image.png'
+import img2 from './images/img2.jpeg'
+import paris from './images/paris.webp'
+import mumbai from './images/mumbai.webp'
+import tokyo from './images/tokyo.webp'
+import rome from './images/rome.webp' 
+import newyork from './images/newyork.jpg'
+import england from './images/england.jpeg'
+
+
+
+export const data = [
+	{
+		id: 1,
+		date: "May 2014",
+		title: "Panajachel, Lake Atitlán",
+		description: 
+"The busiest and most built-up lakeside town, Panajachel ('Pana') is the gateway to Lago de Atitlán for most travelers.",
+		coverImg: img1,
+		country: "GUATEMALA",
+		googleMapLink: 
+"https://www.google.com/maps/place/Lake+Atitl%C3%A1n/@14.6788007,-91.2837663,12z/data=!3m1!4b1!4m6!3m5!1s0x85894ac7c083b493:0xa6e33f7d6b54910!8m2!3d14.6906713!4d-91.2025207!16zL20vMDN3NXpy?entry=ttu",
+	},
+	{
+		id: 2,
+		date: "December 2015",
+		title: "Secret Beach",
+		description: 
+"Experience the beautiful landscape and meet new friends all while enjoying pristine warm waves of the Atlantic.",
+		coverImg: img2,
+		country: "BELIZE",
+		googleMapLink: 
+"https://www.google.com/maps/search/belize+secret+beach/@18.0171292,-87.9672513,13z/data=!3m1!4b1?entry=ttu",
+	},
+	{
+		id: 3,
+		date: "June to August, September to October",
+		title: "City of Love",
+		description: 
+"The City of Light is renowned for its art, fashion, and culture. Stroll along the Seine, visit the Louvre, and enjoy a coffee at a charming café. Eiffel Tower views and gourmet cuisine are unforgettable",
+		coverImg: paris,
+		country: "France",
+		googleMapLink: 
+"https://www.introducingparis.com/map/",
+	},
+	{
+		id: 4,
+		date: "July 2017",
+		title: "Bombay",
+		description: 
+"Experience the beautiful landscape and meet new friends all while enjoying pristine warm waves of the Atlantic.",
+		coverImg: mumbai,
+		country: "India",
+		googleMapLink: 
+"https://www.google.com/maps/place/Mumbai,+Maharashtra/@19.082502,72.7163721,11z/data=!3m1!4b1!4m6!3m5!1s0x3be7c6306644edc1:0x5da4ed8f8d648c69!8m2!3d19.0759837!4d72.8776559!16zL20vMDR2bXA?entry=ttu",
+	},
+	{
+		id: 5,
+		date: "September 2018",
+		title: "The smallest country in the world",
+		description: 
+"Experience the beautiful landscape and meet new friends all while enjoying pristine warm waves of the Atlantic.",
+		coverImg: rome,
+		country: "Italy",
+		googleMapLink: 
+"https://rome.net/map/",
+	},
+	{
+		id: 6,
+		date: "March to May, September to November",
+		title: "Fire Works Fill the night Sky",
+		description: 
+" A city of contrasts, Tokyo blends ancient temples with futuristic skyscrapers. Enjoy the bustling streets of Shibuya, serene gardens, and the historic Asakusa district.",
+		coverImg: tokyo,
+		country: "japan",
+		googleMapLink: 
+"https://www.introducingtokyo.com/map",
+	},{
+		id: 7,
+		date: "April to June, September to early November",
+		title: "Green Apple",
+		description: 
+" The city that never sleeps, New York offers iconic landmarks like Times Square, Central Park, and the Statue of Liberty. Its vibrant arts scene, world-class museums, and diverse culinary options make it a must-visit."
+,
+		coverImg: newyork,
+		country: "USA",
+		googleMapLink: 
+"https://mapcarta.com/New_York_City",
+	},
+	{
+		id: 8,
+		date: "March to May, September to November",
+		title: "Londaon",
+		description: 
+" : With its rich history and modern attractions, London offers something for everyone. Explore the British Museum, Buckingham Palace, and the vibrant markets of Camden Town.."
+,
+		coverImg: england,
+		country: "England",
+		googleMapLink: 
+"https://mapcarta.com/London/Directions",
+	}
+
+
+
+]
